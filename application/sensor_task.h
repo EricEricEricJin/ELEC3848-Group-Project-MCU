60132d0617d3e8345740d3cf03b03bb48705bc9e
Zou 2004 <u3594662@connect.hku.hk>
2024-10-18 13:22:08 +0800

    update error

--- application/sensor_task.h
+++ application/sensor_task.h
@@ -1,11 +1,7 @@
 #ifndef __sensor_TASK_H__
 #define __sensor_TASK_H__
 
-typedef struct SensorData *Sensordata
-
-void sensor_setup();
-void sensor_loop(SensorData sensorData);
-
+typedef struct SensorData *Sensordata;
 
 // Struct to store sensor data
 struct SensorData {
@@ -14,4 +10,7 @@ struct SensorData {
     float angleY;
     float angleZ;
 };
+
+void sensor_setup();
+void sensor_loop(SensorData sensorData);
 #endif
